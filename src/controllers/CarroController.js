0579cc57f77cdac656343eeaf7837f2acfe964c8
Pedro <rogenun52@hotmail.com>
2024-05-14 10:04:43 -0300

    tudo nos conformer

---
+++ src/controllers/CarroController.js
@@ -0,0 +1,1 @@
+const CarrosService = require('../services/CarroServices');
--- src/server.js
+++ src/server.js
@@ -9,8 +9,10 @@ const routes = require('./routes');
 const server = express();
 server.use(cors());
 server.use(bodyParser.urlencoded({extended: false}));
+server.use('/api',routes);
+
 
 server.listen(process.env.PORT,()=>{
     console.log(`Servidor rodando em: http://localhost: ${process.env.PORT}`);
-    //console.log("Servidor rodando em http://localhost: " + )
+    //console.log("Servidor rodando em http://localhost: " + process.env.PORT);//CONCATENAÇÃO
 })
